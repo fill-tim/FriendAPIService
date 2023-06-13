28185ae1cf4e846d020b8e1c0eee3d5f2da972af
Fill <tima.filicheff1@yandex.ru>
2023-06-13 15:27:35 +0300

    добавил новые urls для удаления и определения юзера

--- src/friend/urls.py
+++ src/friend/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import ListFriendsAPI, RequestFriend, IncomingFriendRequests, OutgoingFriendRequests, ResponseToRequest
+from . import views
 
 urlpatterns = [
-    path('list_friends/', ListFriendsAPI.as_view()),
-    path('outgoing_request/', OutgoingFriendRequests.as_view()),
-    path('incoming_request/', IncomingFriendRequests.as_view()),
-    path('send_request/', RequestFriend.as_view()),
-    path('response_to_request/', ResponseToRequest.as_view()),
+    path('list_friends/', views.ListFriendsAPI.as_view()),
+    path('outgoing_request/', views.OutgoingFriendRequests.as_view()),
+    path('incoming_request/', views.IncomingFriendRequests.as_view()),
+    path('send_request/', views.RequestFriends.as_view()),
+    path('response_to_request/', views.ResponseToRequest.as_view()),
+    path('delete_friend/', views.DeleteFriend.as_view()),
+    path('detect_user_state/', views.DetectUsersState.as_view()),
 ]
