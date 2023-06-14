a94ca4cb6141851848500749622611696eb5a703
Fill <tima.filicheff1@yandex.ru>
2023-06-14 18:19:45 +0300

    Создал тест для проверки регистрации пользователей

--- src/authentication/tests.py
+++ src/authentication/tests.py
@@ -5,7 +5,8 @@ from src.friend.models import User
 
 
 class RegisterTestCase(APITestCase):
-    def test_create_account(self):
-        url = reverse('register')
-        response = self.client.get(url)
-        print(response)
+    """ Тестирование регистрации """
+    def test_registration(self):
+        data = {"username": "Fill", "password": "1234"}
+        response = self.client.post(reverse("register"), data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
