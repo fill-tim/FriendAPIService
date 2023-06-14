82539a167af6454f9cf09592e47d59e4739349ef
Fill <tima.filicheff1@yandex.ru>
2023-06-14 18:20:44 +0300

    Добавил name для url

--- src/friend/urls.py
+++ src/friend/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list_friends/', views.ListFriendsAPI.as_view()),
-    path('outgoing_request/', views.OutgoingFriendRequests.as_view()),
-    path('incoming_request/', views.IncomingFriendRequests.as_view()),
-    path('send_request/', views.RequestFriends.as_view()),
-    path('response_to_request/', views.ResponseToRequest.as_view()),
-    path('delete_friend/', views.DeleteFriend.as_view()),
-    path('detect_user_state/', views.DetectUsersState.as_view()),
+    path('list_friends/', views.ListFriendsAPI.as_view(), name="list_friends"),
+    path('outgoing_request/', views.OutgoingFriendRequests.as_view(), name='outgoing_request'),
+    path('incoming_request/', views.IncomingFriendRequests.as_view(), name='incoming_request'),
+    path('send_request/', views.RequestFriends.as_view(), name='send_request'),
+    path('response_to_request/', views.ResponseToRequest.as_view(), name="response_to_request"),
+    path('delete_friend/', views.DeleteFriend.as_view(), name="delete_friend"),
+    path('detect_user_state/', views.DetectUsersState.as_view(), name="detect_users_state"),
 ]
