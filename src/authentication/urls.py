1193221b20c47755fce7b39291675923bdff88ba
Fill <tima.filicheff1@yandex.ru>
2023-06-15 22:58:37 +0300

    добавил urls для авторизации TokenAuthentication

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include, re_path
 
 from .views import RegisterUserAPI
 
 urlpatterns = [
     path('register/', RegisterUserAPI.as_view(), name="register"),
+    path('auth/', include('djoser.urls')),
+    re_path('auth/', include('djoser.urls.authtoken')),
 ]
